811abe10794854f398115a6b8c92ca915b711caf
Omkar Nimase <omkar.nimase20@vit.edu>
2024-03-06 19:52:55 +0530

    project

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1> Hello Omkar !!!</h1>
+  );
+}
+
+export default App;
---
+++ 01viteproject/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <h1>omkar nimase @omnimase </h1>
+  )
+}
+
+export default App
