59bd7eb78b68de8d8b914d62e49a3324e78466ef
Omkar Nimase <omkar.nimase20@vit.edu>
2024-03-15 08:27:20 +0530

    Currency Done

--- 06currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -1,18 +1,16 @@
+import {useEffect, useState} from "react"
 
-import {useState, useEffect} from "react"
 
 function useCurrencyInfo(currency){
-
-    const[data,setdata] = useState({});
-
-    useEffect(()=>{
-        fetch(`https://cdn.jadelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-        .then((res) => res.json)// convert dtring to json
-        .then((res)=> setdata(res[currency]))
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
         console.log(data);
-    },[currency])
+    }, [currency])
     console.log(data);
-    return data;
+    return data
 }
+
 export default useCurrencyInfo;
-// `https://cdn.jadelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
