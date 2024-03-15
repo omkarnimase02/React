ed7e4b3ef81c204bf1482559a351fae0e4609226
Omkar Nimase <omkar.nimase20@vit.edu>
2024-03-15 08:30:41 +0530

    Bg Changer Done

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <h1 className="text-center pd-40  ">Omkar Nimase</h1>
       <div className="fixed flex flex-wrap 
       justify-center bottom-12 inset-x-0 px-2"
-      >
+      >       
         <div className="flex flex-wrap justify-center
         gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
           <button 
@@ -29,6 +29,7 @@ function App() {
           className="outline-none px-4 py-1 rounded-full
           text-white shadow-lg"
           style={{backgroundColor:"green"}}>
+            
             Green</button>
 
             <button 
